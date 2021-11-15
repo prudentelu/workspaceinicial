ca3413c56448e9f60836c2d883b2a11b16779182
prudentelu <lvprudente@gmail.com>
2021-11-14 23:38:54 -0300

    Entrega 7
    Carrito Validaciones y Costo total

--- js/cart.js
+++ js/cart.js
@@ -1,10 +1,13 @@
 var boughtProducts = [];
+let subtotal= 0;
+mensajeExito = [];
+
 
 function showBoughtProducts(array) {
 
     for (let i = 0; i < array.length; i++) {
         let productosComprados = array[i];
-
+        
         var table = document.getElementById("boughtProducts");
 
         var row = table.insertRow(1);
@@ -13,6 +16,7 @@ function showBoughtProducts(array) {
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
         var cell5 = row.insertCell(4);
+        var cell6 = row.insertCell(5);
         
 
         
@@ -23,6 +27,11 @@ function showBoughtProducts(array) {
         cell3.innerHTML = productosComprados.unitCost + productosComprados.currency;
         cell4.innerHTML = `<p id="mostrarSubtotal"> ` + (productosComprados.unitCost * productosComprados.count) + productosComprados.currency + `</p>`
         cell5.innerHTML = `<td><a href="">Quitar</a></td>`;
+        cell6.innerHTML = `<p id="mostrarTotalAPagar"> `
+
+
+        subtotal+= productosComprados.unitCost * productosComprados.count;
+
 
     };
 };
@@ -39,6 +48,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
             showBoughtProducts(boughtProducts.articles);
         };
     });
+    let inputsDatosEnvio= document.getElementsByName('flexRadioDefault');
+    console.log(inputsDatosEnvio);
 });
 
 function calcularSubtotal() {
@@ -47,21 +58,101 @@ function calcularSubtotal() {
     document.getElementById("mostrarSubtotal").innerHTML = cantidad * precio;
 };
 
-function mostrarDatosDeEnvío() {
-    var formularioEnvío = document.getElementById("formularioDatosDeEnvío");
-    formularioEnvío.style.display = "block";
-}
 
 function mostrarFactura() {
-    var formularioFacturación = document.getElementById("formularioFacturación");
-    var imputDirección = document.getElementById("direcciónUsuario").value
-    var imputPaís = document.getElementById("paísUsuario").value
-    var avisoError = document.getElementById("avisoError")
-    if (imputDirección !== '' && imputPaís !== '') {
-        formularioFacturación.style.display = "block";
+    
+    var imputDirección = document.getElementById("direcciónUsuario").value;
+    var imputPaís = document.getElementById("paísUsuario").value;
+    var avisoError = document.getElementById("avisoError");
+    var inputOp1 = document.getElementById("oPremium");
+    var inputOp2 =  document.getElementById("oExpress");
+    var inputOp3 =  document.getElementById("oStandard");
+    var botonPagar = document.getElementById("botonFormaPago");
+
+    if (imputDirección !== '' && imputPaís !== '' && (inputOp1.checked || inputOp2.checked || inputOp3.checked)) {
+        botonPagar.style.display = "block";
         avisoError.style.display = "none";
+    
     }
     else {
         avisoError.style.display = "block";
     };
+};
+
+
+
+function validarDeposito(){
+
+    var deposito = document.getElementById('pagoDeposito');
+    var cedula = document.getElementById('numeroCedulaDeposito');
+    var avisopagar = document.getElementById('avisoPagarAbitab');
+    var avisoerrorpagar = document.getElementById('avisoPagarAbitabError');
+    var seccionPagoTarj = document.getElementById('seccionPagoTarj');
+    var botonConfirmarDeposito = document.getElementById('botonConfirmarDeposito');
+    var botonCompraFinal = document.getElementById('cierreCompra');
+
+    if (deposito.checked == true && cedula.value !== ""){
+    avisopagar.style.display = "block"; 
+    botonCompraFinal.style.display = "block";
+    avisoerrorpagar.style.display = "none";
+    seccionPagoTarj.style.display = "none";
+    botonConfirmarDeposito.style.display = "none";
+}
+        else {
+            avisoerrorpagar.style.display = "block";
+        };
+    };
+
+function validarTarjeta(){
+
+var tarjeta = document.getElementById('tarjeta');
+var nombreyapellido = document.getElementById('nombreyapellido');
+var numeroTarjeta = document.getElementById('numeroTarjeta');
+var vencimiento = document.getElementById('vencimiento');
+var numVerificador = document.getElementById('numverificador');
+var errorPagoTarjeta = document.getElementById('avisoErrorPagoTarj');
+var seccionPagoDep = document.getElementById('seccionPagoDep');
+var botonCompraFinal = document.getElementById('cierreCompra');
+var botonConfirmarTarj = document.getElementById('botonConfirmarTarj');
+
+if (tarjeta.checked == true || nombreyapellido.value !== "" || numeroTarjeta.value !== ""
+ || vencimiento.value !== "" || numVerificador.value !== ""){
+    
+    botonCompraFinal.style.display = "block";
+    seccionPagoDep.style.display = "none";
+    botonConfirmarTarj.style.display = "none";
+    errorPagoTarjeta.style.display = "none";
+
+} else {
+    errorPagoTarjeta.style.display = "block";
+    
+};
+};
+
+
+
+function sumarCostoTotal(){
+    var envio= document.getElementsByName("flexRadioDefault");
+  for(let i = 0; i < envio.length; i++){
+     if (envio[i].checked){
+  let valorenvio = envio[i].value;
+  let sumaenvio= subtotal * valorenvio;
+  document.getElementById("mostrarTotalAPagar").innerHTML= (sumaenvio * 1.22).toFixed(2); 
+     };
+ };
+ };
+
+
+ document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CART_BUY_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            mensajeExito = resultObj.data.msg;
+        }
+    });
+});
+
+function showMensajeFinal(){
+
+    alert(mensajeExito);
+    console.log(mensajeExito);
 };
